9872961325649e686af7e2cad0df50bd87c4e01e
Teddy <brianrcdteodoro@gmail.com>
2019-12-04 19:07:39 +0800

    from static to dynamic using state

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+	state = {
+		people: [
+			{
+				name: 'Yema',
+				age: 3
+			},
+			{
+				name: 'Ube',
+				age: 4
+			},
+			{
+				name: 'Bulalo',
+				age: 5
+			}
+		]
+	};
+
 	render() {
 		return (
 			<div className="App">
 				<h1>I'm from App Component</h1>
-				<Person name="Yema" />
-				<Person name="Ube" />
-				<Person name="Bulalo" />
+				<button>Change Name</button>
+				<Person name={this.state.people[0].name} age={this.state.people[0].age} />
+				<Person name={this.state.people[1].name} age={this.state.people[1].age}>
+					Anak ni Ube
+				</Person>
+				<Person name={this.state.people[2].name} age={this.state.people[2].age} />
 			</div>
 		);
 	}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ const Person = (props) => {
 	return (
 		<div>
 			<h3>{props.name}</h3>
+			<p>{props.children}</p>
 			<p>I just used props to access the stuff from App component</p>
 		</div>
 	);
